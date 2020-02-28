02638169e68a811a62a07d9f08e4f19bb99b5c9f
rcp-dev <ryanpufferdev@gmail.com>
2020-02-28 10:28:03 -0600

    set up passport

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-var express = require('express');
-    mongoose = require('mongoose');
+var express                 = require('express'),
+    mongoose                = require('mongoose');
+    passport                = require('passport'),
+    bodyParser              = require('body-parser'),
+    User                    = require('./models/user')
+    LocalStrategy           = require('passport-local'),
+    passportLocalMongoose   = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://localhost/auth_demo_app");
 
+mongoose.connect("mongodb://localhost/auth_demo_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 var app = express();
+
+app.use(require("express-session")({
+    secret: "Redford is my son",
+    resave: false,
+    saveUninitialized: false
+}))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize()); //need these two lines anytime use passport
+app.use(passport.session());  //need these two lines anytime use passport
 
 app.get("/", function(req, res){
     res.render("home");
