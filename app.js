7267a347929ca0a0279c12b3593f984088ffc6e8
rcp-dev <ryanpufferdev@gmail.com>
2020-02-29 20:34:12 -0600

    add login/logout route and created middleware

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use(require("express-session")({
     resave: false,
     saveUninitialized: false
 }))
+app.use(bodyParser.urlencoded({extended: true}));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -36,13 +38,56 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 })
 
 app.get("/register", function(req, res){
     res.render("register");
 })
+
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            })
+        }
+    })
+})
+
+//LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+
+})
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("login");
+}
 
 app.listen(3000, function(){
     console.log("Server has started!")
---
+++ readme.md
@@ -0,0 +1,1 @@
+Practice with Authentication using Passport
